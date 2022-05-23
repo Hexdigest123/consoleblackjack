134f7b31004a5f4a712f75334cc6d462850ace00
Hexdigest <merckelpierremaurice@gmail.com>
2022-05-23 16:00:49 +0200

    Revert "Revert "- First Commit -""
    This reverts commit 9d937bd04e0511874aa158f003bc9e2263643469.

---
+++ blackjack/Player.cs
@@ -0,0 +1,56 @@
+﻿namespace blackjack
+{
+    public class Player
+    {
+        // Constructor & Destructor
+
+        public Player(bool isDealer)
+        {
+            _balance = isDealer ? 1000000 : 1000;
+            _cards = new Card[2];
+        }
+        
+        // Functions
+
+        public void ExtendArray() // Pass by reference
+        {
+            Card[] localArray = new Card[_cards.Length];
+            for (int i = 0; i < _cards.Length; i++)
+            {
+                localArray[i] = _cards[i];
+            }
+
+            _cards = new Card[_cards.Length + 1];
+            localArray = null;
+        }
+
+        public void AddCard(Card card)
+        {
+            if (_cardsRealLength == _cards.Length - 1)
+            {
+                ExtendArray();
+            }
+            _cards[_cardsRealLength] = card;
+            _cardsRealLength++;
+        }
+        
+        
+        // getter & setter
+        public void DecreaseMoney(int decreaseBy) { _balance -= decreaseBy; }
+        public void IncreaseMoney(int increaseBy) { _balance += increaseBy;}
+
+        public void SetBet(int bet) { _bet = bet; }
+        public int GetBet() { return _bet; }
+        
+        public int GetBalance() { return _balance; }
+        public void SetBalance(int balance) { _balance = balance; }
+        
+        // Private variables
+
+        private int _bet;
+        private int _balance;
+        private Card[] _cards;
+        private int _cardsRealLength = 0;
+
+    }
+}
---
+++ blackjack/Table.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace blackjack
+{
+    public class Table
+    {
+        
+        // Constructor & Destructor
+
+        public Table()
+        {
+            Console.WriteLine("[DEBUG] reserve memory for Table");
+            GameLoop();
+        }
+        
+        ~Table()
+        {
+            Console.WriteLine("[DEBUG] free memory of Table");
+        }
+        
+        // Functions 
+
+        private int CountCards(Card[] array)
+        {
+            return 1;
+        }
+        
+        private void PrintCards(Card[] array)
+        {
+            
+        }
+        
+        private void SortCards(ref Card[] array)
+        {
+            // selection sort
+            Console.WriteLine("Your Cards: ");
+            foreach (var card in array)
+            {
+                Console.Write($"");
+            }
+            
+        }
+        
+        private void GameLoop()
+        {
+            while (!_gameStarted)
+            {
+                Console.Write("\t\tWelcome to Blackjack\n\nDo you want to play [yes,no]: ");
+                if (Console.ReadLine() == "yes")
+                {
+                    _gameStarted = true;
+                }
+            }
+            
+            _player = new Player(false);
+            _headlessDealer = new Player(true);
+            
+            while (_gameStarted)
+            {
+                ClearConsole();
+                Console.WriteLine($"Your current balance: {Convert.ToString(_player.GetBalance())}\n\n");
+
+                Console.Write("Welcome to your seat, how much you want to bet [only integer values]: ");
+                try
+                {
+                    int playerBet = Convert.ToInt32(Console.ReadLine());
+                    _player.SetBet(playerBet);
+                    _headlessDealer.SetBet(playerBet*2);
+                }
+                catch (InvalidCastException)
+                {
+                    Console.WriteLine("!pls read the the instructions!\n");
+                    System.Threading.Thread.Sleep(1000);
+                    continue;
+                }
+                
+                ClearConsole();
+                Console.WriteLine($"Your bet is: {Convert.ToString(_player.GetBet())}\n");
+
+                Random rand = new Random();
+                _player.AddCard(new Card(rand.Next(4), rand.Next(12)));
+                _player.AddCard(new Card(rand.Next(4), rand.Next(12)));
+
+                _headlessDealer.AddCard(new Card(rand.Next(4), rand.Next(12)));
+                _headlessDealer.AddCard(new Card(rand.Next(4), rand.Next(12)));
+                
+                
+                
+            }
+        }
+        
+        private void ClearConsole(){ for(var i = 0; i < 150; i++){ Console.WriteLine(); }}
+        
+        // Private variables
+
+        private bool _gameStarted = false;
+        
+        private Player _player;
+        private Player _headlessDealer;
+        
+    }
+}
